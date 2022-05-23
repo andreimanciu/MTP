6e2c75f01d0cf7584da1bdc858a31187f1ffa7c5
andreimanciu <79370729+andreimanciu@users.noreply.github.com>
2022-05-23 08:44:05 +0300

    5

---
+++ Laborator5/Models/BibliotecaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Laborator5.Models
+{
+    public class BibliotecaContext: DbContext
+    {
+        public BibliotecaContext() : base("BibliotecaContext")
+        {
+        }
+
+        public virtual DbSet<Client> Clienti { get; set; }
+        public virtual DbSet<Carte> Carti { get; set; }
+        public virtual DbSet<Imprumut> Imprumuturi { get; set; }
+
+    }
+}
---
+++ Laborator5/Models/Carte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Laborator5.Models
+{
+    public class Carte
+    {
+        public int CarteId { get; set; }
+        public string Titlu { get; set; }
+        public string Autor { get; set; }
+        public string Editura { get; set; }
+
+        [Range(1901, 2021), Display(Name = "Anul aparitiei")]
+        public int AnAparitie { get; set; }
+    }
+}
